163a3ea780290af14ccd70980db13e0debe4de42
CancuiW <can.cui@wayne.edu>
2023-08-30 14:22:18 -0700

    finished

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+const port=process.env.PORT||9000
+
 const express = require('express');
 const server = express();
 
@@ -11,6 +14,6 @@ server.use('*', (req, res) => {
   res.json({ message: 'API is UP!' })
 });
 
-server.listen(9000, () => {
-  console.log("\n*** Server Running on http://localhost:9000 ***\n");
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
